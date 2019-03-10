981840a78565e4fe4cfe79cd181d63d411b83489
rmakerck37 <rmakerck37@example.com>
2019-03-10 22:14:09 +0900

    fourth commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,6 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
